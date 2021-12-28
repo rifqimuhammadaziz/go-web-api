2639548564f186effd82c4aa165a1b673b93b2b0
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-28 19:49:38 +0700

    add url and query parameter

--- main.go
+++ main.go
@@ -10,8 +10,9 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", rootHandler)
-
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
 
 	// change listening port
 	router.Run(":8000")
@@ -29,5 +30,25 @@ func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"title":       "Hello World",
 		"description": "Ini adalah hello world",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")       // get id from url parameter (localhost:8000/books/2)
+	title := c.Param("title") // get title from url parameter (localhost:8000/books/2/ini-adalah-judul)
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	// localhost:8000/query?price=40&title=ini adalah judul buku
+	title := c.Query("title")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{
+		"price": price,
+		"title": title,
 	})
 }
