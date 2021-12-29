d4c07f3bb9fc6a2d4de94fe54f53334badb4cdca
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2021-12-29 14:51:00 +0700

    create entity for migration db, connect db & test create

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"myweb-api/book"
 	"myweb-api/handler"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	// https://github.com/go-sql-driver/mysql
+	dsn := "root:root@tcp(localhost:3306)/myweb_api?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Db connection error")
+	}
+
+	// migrate struct to database
+	db.AutoMigrate(&book.Book{})
+
+	// create every running server
+	book := book.Book{}
+	book.Title = "Buku kedua"
+	book.Price = 120000
+	book.Rating = 5
+	book.Description = "Ini adalah deskripsi buku kedua"
+
+	err = db.Create(&book).Error
+	if err != nil {
+		fmt.Println("=========================")
+		fmt.Println("Failed create book record")
+		fmt.Println("=========================")
+	}
+
 	router := gin.Default()
 
 	// API Versioning
